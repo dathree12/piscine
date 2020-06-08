2a45572f53ddfa4ad23f34eba54e0ac8538de69d
Daseul Park <daspark@c4r1s7.42seoul.kr>
2020-06-08 22:21:23 +0900

    C03edit

--- piscinegit/C03/ex03/ft_strncat.c
+++ piscinegit/C03/ex03/ft_strncat.c
@@ -6,9 +6,21 @@
 /*   By: daspark <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/08 15:37:26 by daspark           #+#    #+#             */
-/*   Updated: 2020/06/08 21:23:31 by daspark          ###   ########.fr       */
+/*   Updated: 2020/06/08 22:13:59 by daspark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+int		ft_length(char *dest)
+{
+	int i;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
 
 char	*ft_strncat(char *dest, char *src, unsigned int nb)
 {
@@ -16,15 +28,15 @@ char	*ft_strncat(char *dest, char *src, unsigned int nb)
 	int i_src;
 	int leng;
 
-	leng = ft_leng(dest);
 	i_dest = 0;
 	i_src = 0;
+	leng = ft_length(dest);
 	while (i_src < nb && src[i_src] != '\0')
 	{
-		dest[i_dest] = src[i_src];
+		dest[i_dest + leng] = src[i_src];
 		i_dest++;
 		i_src++;
 	}
-	dest[i_dest] = '\0';
+	dest[i_dest + leng] = '\0';
 	return (dest);
 }
--- piscinegit/C03/main3.c
+++ piscinegit/C03/main3.c
@@ -6,44 +6,52 @@
 /*   By: daspark <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/08 15:46:35 by daspark           #+#    #+#             */
-/*   Updated: 2020/06/08 16:12:36 by daspark          ###   ########.fr       */
+/*   Updated: 2020/06/08 22:12:48 by daspark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
+int	ft_length(char *dest)
+{
+	int i;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
 char	*ft_strncat(char *dest, char *src, unsigned int nb)
 {
 	int i_dest;
 	int i_src;
+	int leng;
 
 	i_dest = 0;
 	i_src = 0;
-	while (dest[i_dest] != '\0')
-		i_dest++;
-	while (i_src < nb && src[i_src] != '\0')
+	leng = ft_length(dest);
+	while (i_src < nb || src[i_src] != '\0')
 	{
-		dest[i_dest] = src[i_src];
+		dest[i_dest + leng] = src[i_src];
 		i_dest++;
 		i_src++;
 	}
-	dest[i_dest] = '\0';
+	dest[i_dest + leng] = '\0';
 	return (dest);
 }
 
 int main()
 {
 
-	char a[30] = "apple pie";
-	char b[30] = "banana pie";
-	char c[30] = "";
-	char d[30] = "good";
+	char a[30] = "123";
+	char b[30] = "45678";
+	char c[30] = "4";
+	char d[30] = "456";
 
-	printf("%s \n", ft_strncat(a, b, 10));
-	printf("%s \n", ft_strncat(c, b, 5));
-	printf("%s \n", ft_strncat(a, c, 3));
-	printf("%s \n", ft_strncat(d, b, 2));
-	printf("%s \n", ft_strncat(b, d, 8));
-	printf("%s \n", ft_strncat(a, d, 0));
-	return (0);
+	printf("%s \n", ft_strncat(a, b, 3));
+	printf("%s \n", ft_strncat(b, c, 3));
+	printf("%s \n", ft_strncat(c, d, 3));
 }	
